34a98997ef71fe0e8d091eb65f55971891f12f36
i-walker <46971368+i-walker@users.noreply.github.com>
2021-11-28 16:56:23 +0100

    fix

--- kotest-assertions-arrow/build.gradle.kts
+++ kotest-assertions-arrow/build.gradle.kts
@@ -194,7 +194,7 @@ kotlin {
     val linuxX64Main by getting
     val iosArm32Main by getting
     val iosArm64Main by getting
-    // val iosSimulatorArm64Main by getting
+    val iosSimulatorArm64Main by getting
     val iosX64Main by getting
     val macosArm64Main by getting
     val macosX64Main by getting
@@ -213,7 +213,7 @@ kotlin {
       linuxX64Main.dependsOn(this)
       iosArm32Main.dependsOn(this)
       iosArm64Main.dependsOn(this)
-      // iosSimulatorArm64Main.dependsOn(this)
+      iosSimulatorArm64Main.dependsOn(this)
       iosX64Main.dependsOn(this)
       macosArm64Main.dependsOn(this)
       macosX64Main.dependsOn(this)
