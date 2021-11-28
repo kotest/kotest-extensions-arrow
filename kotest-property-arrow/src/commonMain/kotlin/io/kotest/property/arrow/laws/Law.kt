7c41a133b83e029a7ac55f80f3aff07ba570fb87
i-walker <46971368+i-walker@users.noreply.github.com>
2021-11-28 17:12:42 +0100

    fix compile error

--- kotest-property-arrow/src/commonMain/kotlin/io/kotest/property/arrow/laws/Law.kt
+++ kotest-property-arrow/src/commonMain/kotlin/io/kotest/property/arrow/laws/Law.kt
@@ -2,7 +2,8 @@ package io.kotest.property.arrow.laws
 
 import io.kotest.assertions.fail
 import io.kotest.core.names.TestName
-import io.kotest.core.spec.style.scopes.RootContext
+import io.kotest.core.spec.style.scopes.RootScope
+import io.kotest.core.spec.style.scopes.addTest
 import io.kotest.core.test.TestContext
 
 public data class Law(val name: String, val test: suspend TestContext.() -> Unit)
@@ -10,18 +11,18 @@ public data class Law(val name: String, val test: suspend TestContext.() -> Unit
 public fun <A> A.equalUnderTheLaw(b: A, f: (A, A) -> Boolean = { a, b -> a == b }): Boolean =
   if (f(this, b)) true else fail("Found $this but expected: $b")
 
-public fun RootContext.testLaws(vararg laws: List<Law>): Unit =
+public fun RootScope.testLaws(vararg laws: List<Law>): Unit =
   laws
     .flatMap { list: List<Law> -> list.asIterable() }
     .distinctBy { law: Law -> law.name }
     .forEach { law ->
-      registration().addTest(TestName(law.name), xdisabled = false, law.test)
+      addTest(TestName(law.name), disabled = false, config = null) { law.test(this) }
     }
 
-public fun RootContext.testLaws(prefix: String, vararg laws: List<Law>): Unit =
+public fun RootScope.testLaws(prefix: String, vararg laws: List<Law>): Unit =
   laws
     .flatMap { list: List<Law> -> list.asIterable() }
     .distinctBy { law: Law -> law.name }
     .forEach { law: Law ->
-      registration().addTest(TestName(prefix, law.name, true), xdisabled = false, law.test)
+      addTest(TestName(prefix, law.name, true), disabled = false, config = null) { law.test(this) }
     }
