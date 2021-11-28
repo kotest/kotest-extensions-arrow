c8c32a5a499727da3c589466ff7b634346098800
i-walker <46971368+i-walker@users.noreply.github.com>
2021-11-28 17:28:45 +0100

    remove not needed dependencies, and align shouldBe from assertions

--- kotest-property-arrow/build.gradle.kts
+++ kotest-property-arrow/build.gradle.kts
@@ -18,7 +18,6 @@ kotlin {
     val commonMain by getting {
       dependencies {
         compileOnly("org.jetbrains.kotlin:kotlin-stdlib-common:1.6.0")
-        compileOnly("io.kotest:kotest-assertions-shared:5.0.0")
         compileOnly("io.kotest:kotest-assertions-core:5.0.0")
         compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC")
         compileOnly("io.kotest:kotest-framework-api:5.0.0")
