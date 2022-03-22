1b77cf62f5a15ed9c72071ca5d11cddeeac4d5ff
Mitchell Yuwono <myuwono@atlassian.com>
2022-03-22 23:49:38 +1100

    use ContainerScope for testLaws, test registration after spec instantiation is no longer legal

--- kotest-property-arrow/src/commonMain/kotlin/io/kotest/property/arrow/laws/Law.kt
+++ kotest-property-arrow/src/commonMain/kotlin/io/kotest/property/arrow/laws/Law.kt
@@ -1,8 +1,8 @@
 package io.kotest.property.arrow.laws
 
 import io.kotest.assertions.fail
-import io.kotest.core.spec.DslDrivenSpec
-import io.kotest.core.spec.style.funSpec
+import io.kotest.core.names.TestName
+import io.kotest.core.spec.style.scopes.ContainerScope
 import io.kotest.core.test.TestScope
 
 public data class Law(val name: String, val test: suspend TestScope.() -> Unit)
@@ -10,22 +10,18 @@ public data class Law(val name: String, val test: suspend TestScope.() -> Unit)
 public fun <A> A.equalUnderTheLaw(other: A, f: (A, A) -> Boolean = { a, b -> a == b }): Boolean =
   if (f(this, other)) true else fail("Found $this but expected: $other")
 
-public fun DslDrivenSpec.testLaws(vararg laws: List<Law>): Unit =
-  include(funSpec {
-    laws
-      .flatMap { list: List<Law> -> list.asIterable() }
-      .distinctBy { law: Law -> law.name }
-      .forEach { law ->
-        test(law.name) { law.test(this) }
-      }
-  })
+public suspend fun ContainerScope.testLaws(vararg laws: List<Law>): Unit =
+  laws
+    .flatMap { list: List<Law> -> list.asIterable() }
+    .distinctBy { law: Law -> law.name }
+    .forEach { law ->
+      registerTest(TestName(law.name), disabled = false, config = null) { law.test(this) }
+    }
 
-public fun DslDrivenSpec.testLaws(prefix: String, vararg laws: List<Law>): Unit =
-  include(funSpec {
-    laws
-      .flatMap { list: List<Law> -> list.asIterable() }
-      .distinctBy { law: Law -> law.name }
-      .forEach { law: Law ->
-        test("$prefix ${law.name}") { law.test(this) }
-      }
-  })
+public suspend fun ContainerScope.testLaws(prefix: String, vararg laws: List<Law>): Unit =
+  laws
+    .flatMap { list: List<Law> -> list.asIterable() }
+    .distinctBy { law: Law -> law.name }
+    .forEach { law: Law ->
+      registerTest(TestName(prefix, law.name, true), disabled = false, config = null) { law.test(this) }
+    }
--- kotest-property-arrow/src/commonTest/kotlin/io/kotest/property/arrow/core/MonoidLawTests.kt
+++ kotest-property-arrow/src/commonTest/kotlin/io/kotest/property/arrow/core/MonoidLawTests.kt
@@ -1,15 +1,15 @@
 package io.kotest.property.arrow.core
 
 import arrow.typeclasses.Monoid
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.boolean
 import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.string
 import io.kotest.property.arrow.laws.testLaws
 
-class MonoidLawTests : StringSpec({
-  "Boolean, Int, String obeys MonoidLaws" {
+class MonoidLawTests : FunSpec({
+  context("Boolean, Int, String obeys MonoidLaws") {
     testLaws(
       MonoidLaws.laws(Monoid.boolean(), Arb.boolean()),
       MonoidLaws.laws(Monoid.int(), Arb.int()),
