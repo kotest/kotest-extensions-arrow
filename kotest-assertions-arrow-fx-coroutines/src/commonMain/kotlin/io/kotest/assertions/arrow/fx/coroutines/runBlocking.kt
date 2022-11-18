357bc227bd91ae6e486ea99c402995e8d7ee6d4f
i-walker <46971368+i-walker@users.noreply.github.com>
2022-11-18 13:34:53 +0100

    prepare branch for 1.3.0

--- kotest-assertions-arrow-fx-coroutines/src/commonMain/kotlin/io/kotest/assertions/arrow/fx/coroutines/runBlocking.kt
+++
@@ -1,9 +0,0 @@
-package io.kotest.assertions.arrow.fx.coroutines
-
-import kotlinx.coroutines.CoroutineScope
-import kotlin.coroutines.CoroutineContext
-
-internal expect fun <T> runBlocking(
-  context: CoroutineContext,
-  block: suspend CoroutineScope.() -> T
-): T
